2963db01cc01196eb5b220193541f187d7761e26
Jason H <jasonhartantoo14@gmail.com>
2023-10-15 21:34:35 +0700

    add layout, tailwind

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h1 class="font-semibold text-4xl text-blue-400">Welcome to the Home Page</h1>
+    <p>This is the content of the home page.</p>
+@endsection
