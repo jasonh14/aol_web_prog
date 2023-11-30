99747d009a098f1c927e3ec3bf1adf09d3270e3b
Jason H <jasonhartantoo14@gmail.com>
2023-11-30 21:37:16 +0700

    finish your reviews

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chatbot;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,7 +17,8 @@ class DashboardController extends Controller
             $user = Auth::user();
             $user_id = $user->id;
             $chatbots = Chatbot::where('user_id', $user_id)->get();
-            return view('dashboard', ['user' => $user, 'chatbots' => $chatbots]);
+            $comments = Comment::where("user_id", $user_id)->get();
+            return view('dashboard', ['user' => $user, 'chatbots' => $chatbots, "comments" => $comments]);
             // Do something with the $user object
         } else {
             // No user is logged in
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -70,7 +70,7 @@
 
                             <th>
                                 <div class="card-actions justify-end">
-                                    <a href="{{ route('chat', 1) }}">
+                                    <a href="{{ route('chat', $chatbot->id) }}">
                                         <button class="btn btn-primary">Test</button>
                                     </a>
                                 </div>
@@ -94,109 +94,30 @@
                 </thead>
                 <tbody>
                     <!-- row 1 -->
-                    <tr>
-                        <td>
-                            <div class="flex items-center gap-3">
-                                <div class="avatar">
-                                    <div class="mask mask-squircle w-12 h-12">
-                                        <img src="https://source.unsplash.com/user/wsanter"
-                                            alt="Avatar Tailwind CSS Component" />
+                    @foreach ($comments as $comment)
+                        <tr>
+                            <td>
+                                <div class="flex items-center gap-3">
+                                    <div class="avatar">
+                                        <div class="mask mask-squircle w-12 h-12">
+                                            <img src="{{ $comment->chatbot->image_url }}"
+                                                alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">{{ $comment->chatbot->chatbot_name }}</div>
+
                                     </div>
                                 </div>
-                                <div>
-                                    <div class="font-bold">Hart Hagerty</div>
-                                    <div class="text-sm opacity-50">United States</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Zemlak, Daniel and Leannon
-                            <br />
-                            <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                        </td>
-                        <td>Purple</td>
-                        <th>
-                            <button class="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
-                    <!-- row 2 -->
-                    <tr>
-                        <td>
-                            <div class="flex items-center gap-3">
-                                <div class="avatar">
-                                    <div class="mask mask-squircle w-12 h-12">
-                                        <img src="https://source.unsplash.com/user/wsanter"
-                                            alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="font-bold">Brice Swyre</div>
-                                    <div class="text-sm opacity-50">China</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Carroll Group
-                            <br />
-                            <span class="badge badge-ghost badge-sm">Tax Accountant</span>
-                        </td>
-                        <td>Red</td>
-                        <th>
-                            <button class="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
-                    <!-- row 3 -->
-                    <tr>
-                        <td>
-                            <div class="flex items-center gap-3">
-                                <div class="avatar">
-                                    <div class="mask mask-squircle w-12 h-12">
-                                        <img src="https://source.unsplash.com/user/wsanter"
-                                            alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="font-bold">Marjy Ferencz</div>
-                                    <div class="text-sm opacity-50">Russia</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Rowe-Schoen
-                            <br />
-                            <span class="badge badge-ghost badge-sm">Office Assistant I</span>
-                        </td>
-                        <td>Crimson</td>
-                        <th>
-                            <button class="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
-                    <!-- row 4 -->
-                    <tr>
-                        <td>
-                            <div class="flex items-center gap-3">
-                                <div class="avatar">
-                                    <div class="mask mask-squircle w-12 h-12">
-                                        <img src="https://source.unsplash.com/user/wsanter"
-                                            alt="Avatar Tailwind CSS Component" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <div class="font-bold">Yancy Tear</div>
-                                    <div class="text-sm opacity-50">Brazil</div>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            Wyman-Ledner
-                            <br />
-                            <span class="badge badge-ghost badge-sm">Community Outreach Specialist</span>
-                        </td>
-                        <td>Indigo</td>
-                        <th>
-                            <button class="btn btn-ghost btn-xs">details</button>
-                        </th>
-                    </tr>
+                            </td>
+                            <td>
+                                {{ $comment->content }}
+
+                            </td>
+
+                        </tr>
+                    @endforeach
+
                 </tbody>
 
 
